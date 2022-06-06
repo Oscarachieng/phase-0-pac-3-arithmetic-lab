b5711272505d38ee4cbd7b5b943e1db46103db2c
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-06 22:28:59 +0300

    commit tree 9bf08fd017eb501f77234df37d5550de5f6b98cb

---
+++ index.js
@@ -0,0 +1,44 @@
+// Calculator Challenge.
+//function add()
+function add(a, b) {
+ //Put the function body here. 
+ return a + b; 
+}
+add (1,2);
+
+
+//function add()
+function subtract(a ,b) {
+    //Put the function body here. 
+    return a - b;  
+}
+subtract(3, 2);
+   //function add()
+function multiply(a ,b) {
+    //Put the function body here. 
+    return a * b;  
+   }
+multiply(2,3);   
+ 
+   //function add()
+function divide(a, b) {
+    //Put the function body here. 
+    return a / b;  
+   }
+divide (5, 2);
+x = divide (5, 2);
+console.log(x);
+
+//Increment and Decrement functions.
+function increment(n) {
+    let anyNumber = 0;
+    //put the function body here
+    return anyNumber++;
+};
+
+//
+function decrement(n) {
+    //put the function body here
+    let anyNumber = 0;
+    return anyNumber--;
+};
